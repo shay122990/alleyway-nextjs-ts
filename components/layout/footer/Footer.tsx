cf132a4f81661800641c970153fd16e72a4d9fb1
Shay <moeshakasanova@gmail.com>
2024-08-06 19:41:11 +0400

    added links container to footer layout

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -16,11 +16,34 @@ export default function Footer() {
           <Icons/>
         </div>
       </div>
+      <div className={styles.companyContactContainer}>
       <div className={styles.quoteContainer}>
         <h3>Ready to launch your next project?</h3>
         <p>With lots of unique blocks, you can easily build a page without coding. Build your next landing page.</p>
         <Button />
       </div>
+      <div className={styles.linksContainer}>
+        <div className={styles.company}>
+          <h4>Company</h4>
+          <ul>
+            <li>About Us</li>
+            <li>Contact Us</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className={styles.services}>
+          <h4>Services</h4>
+          <ul>
+            <li>Exhibitions & Trade Shows</li>
+            <li>Private Events</li>
+            <li>Creative Services</li>
+            <li>Audio & Visual</li>
+          </ul>
+        </div>
+      </div>
+      </div>
+      
+      
     </div>
   )
 }
