93e32b206b8753e2aa623ac5df33a4db2958f52f
Shay <moeshakasanova@gmail.com>
2024-08-07 17:37:04 +0400

    whatsApp activation, hover styles

--- components/common/whatsapp/WhatsApp.tsx
+++ components/common/whatsapp/WhatsApp.tsx
@@ -1,11 +1,23 @@
-import styles from "./WhatsApp.module.css"
+import React from 'react';
+import styles from "./WhatsApp.module.css";
 import { FaWhatsapp } from "react-icons/fa";
+
 const WhatsApp = () => {
+  const whatsappURL = "https://wa.me/1234567890"; 
+
+  const handleClick = () => {
+    window.open(whatsappURL, '_blank', 'noopener,noreferrer');
+  };
+
   return (
-    <div className={styles.whatsapp}>
-      <FaWhatsapp/>
+    <div 
+      className={styles.whatsapp} 
+      onClick={handleClick}
+      aria-label="Open WhatsApp"
+    >
+      <FaWhatsapp />
     </div>
-  )
+  );
 }
 
-export default WhatsApp
+export default WhatsApp;
