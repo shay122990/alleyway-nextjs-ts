62d66bc6bfa44c60804e650b815375b9b4155b0e
Shay <moeshakasanova@gmail.com>
2024-09-13 17:58:06 +0400

    hide form on submit to show success or failure message

--- components/common/contact-form/Contact-Form.tsx
+++ components/common/contact-form/Contact-Form.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import styles from './Contact-Form.module.css';
 import { FC, useState } from 'react';
@@ -7,7 +6,6 @@ import { sendEmail } from '@/utils/send-email';
 import Button from '../buttons/Button';
 import MessageDisplay from '../message-display/Message-Display';
 
-
 export type FormData = {
   name: string;
   email: string;
@@ -15,25 +13,25 @@ export type FormData = {
 };
 
 const ContactForm: FC = () => {
-  const { register, handleSubmit } = useForm<FormData>();
+  const { register, handleSubmit, reset } = useForm<FormData>();
   const [message, setMessage] = useState<{ text: string; type: 'success' | 'error' } | null>(null);
 
   async function onSubmit(data: FormData) {
     try {
       await sendEmail(data);
       setMessage({ text: 'Your message was sent successfully!', type: 'success' });
+      reset(); 
     } catch (error) {
       setMessage({ text: 'There was an error sending your message. Please try again.', type: 'error' });
     }
   }
 
   const handleCloseMessage = () => {
-    setMessage(null);
+    setMessage(null); 
   };
 
   return (
     <div className={styles.formContainer}>
-      {message && <MessageDisplay message={message.text} type={message.type} onClose={handleCloseMessage} />}
       <div className={styles.contactInfo}>
         <h2 className={styles.formTitle}>LET US TAKE IT FROM HERE ...</h2>
         <div className={styles.formDescription}>
@@ -47,44 +45,51 @@ const ContactForm: FC = () => {
           </p>
         </div>
       </div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.contactForm}>
-        <div className={styles.field}>
-          <label htmlFor='name' className={styles.label}>
-            Full Name
-          </label>
-          <input
-            type='text'
-            placeholder='Name'
-            className={styles.input}
-            {...register('name', { required: true })}
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor='email' className={styles.label}>
-            Email Address
-          </label>
-          <input
-            type='email'
-            placeholder='example@domain.com'
-            className={styles.input}
-            {...register('email', { required: true })}
-          />
-        </div>
-        <div className={styles.field}>
-          <label htmlFor='message' className={styles.label}>
-            Message
-          </label>
-          <textarea
-            rows={5}
-            placeholder='How can we assist you?'
-            className={styles.inputMessage}
-            {...register('message', { required: true })}
-          ></textarea>
-        </div>
-        <div>
-          <Button buttonType="submit">Submit</Button>
-        </div>
-      </form>
+
+      {!message && (
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.contactForm}>
+          <div className={styles.field}>
+            <label htmlFor='name' className={styles.label}>
+              Full Name
+            </label>
+            <input
+              type='text'
+              placeholder='Name'
+              className={styles.input}
+              {...register('name', { required: true })}
+            />
+          </div>
+          <div className={styles.field}>
+            <label htmlFor='email' className={styles.label}>
+              Email Address
+            </label>
+            <input
+              type='email'
+              placeholder='example@domain.com'
+              className={styles.input}
+              {...register('email', { required: true })}
+            />
+          </div>
+          <div className={styles.field}>
+            <label htmlFor='message' className={styles.label}>
+              Message
+            </label>
+            <textarea
+              rows={5}
+              placeholder='How can we assist you?'
+              className={styles.inputMessage}
+              {...register('message', { required: true })}
+            ></textarea>
+          </div>
+          <div>
+            <Button buttonType="submit">Submit</Button>
+          </div>
+        </form>
+      )}
+
+      {message && (
+        <MessageDisplay message={message.text} type={message.type} onClose={handleCloseMessage} />
+      )}
     </div>
   );
 };
