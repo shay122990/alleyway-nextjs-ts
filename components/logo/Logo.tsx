ff15b4788cad7ae25816a250079bb7339be3001d
Shay <moeshakasanova@gmail.com>
2024-09-18 14:22:28 +0400

    logo to navigate to home onClick" git push origin main

--- components/logo/Logo.tsx
+++ components/logo/Logo.tsx
@@ -1,10 +1,20 @@
-import styles from "./Logo.module.css"
-import Image from 'next/image'
-import logoImg from "@/public/images/alleyway-1.png"
+"use client"
+import { useRouter } from 'next/navigation';
+import styles from "./Logo.module.css";
+import Image from 'next/image';
+import logoImg from "@/public/images/alleyway-1.png";
+
 export default function Logo() {
+  const router = useRouter();
+
+
+  const handleClick = () => {
+    router.push('/');  
+  };
+
   return (
-    <div className={styles.logoContainer}>
+    <div className={styles.logoContainer} onClick={handleClick}>
       <Image src={logoImg} alt='Company logo'/>
     </div>
-  )
+  );
 }
