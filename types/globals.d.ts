ce3dd7affd231817bbf08a95991bdd6e4f7d12e0
Shay <moeshakasanova@gmail.com>
2024-09-18 14:18:36 +0400

    defining Image in global ts

--- types/globals.d.ts
+++ types/globals.d.ts
@@ -3,4 +3,22 @@ declare module '*.css' {
     const content: { [className: string]: string };
     export default content;
   }
+  declare module 'next/image' {
+    export interface StaticImageData {
+      src: string;
+      height: number;
+      width: number;
+      blurDataURL?: string;
+    }
+  
+    const Image: React.FC<{
+      src: string | StaticImageData;
+      alt: string;
+      layout?: 'fill' | 'fixed' | 'intrinsic' | 'responsive';
+      objectFit?: string;
+      priority?: boolean;
+    }>;
+  
+    export default Image;
+  }
   
