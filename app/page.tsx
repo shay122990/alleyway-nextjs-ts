b0bd108dde0a2b918d10f5fcdedbf2617f91307d
Shay <moeshakasanova@gmail.com>
2024-09-19 14:07:55 +0400

    events section moved to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ import Head from 'next/head';
 import type { StaticImageData } from 'next/image';
 
 import  homeImage from  "@/public/images/header-home.jpg"
+import projectsImage from "@/public/images/projects-image.jpg";
+
+import EventsSection from "./components/events-section/Events-Section";
 import HomeCarousel from "@/components/layout/home-carousel/Home-Carousel";
 
 type HeroProps = {
@@ -20,7 +23,59 @@ export default function Home() {
         titleClassName={styles.homeTitle}
         paragraphClassName={styles.homeParagraph}/>
       <main>
+      <div className={styles.eventsContainer}>
+          <EventsSection
+            title="Exhibitions & Trade Shows"
+            imageSrc1={projectsImage}
+            links={[
+              { name: 'View Details', href: '/exhibitions#details' },
+              { name: 'Book Now', href: '/exhibitions#book' }
+            ]}
+            className={styles.exhibitionsSection}
+            image1ClassName={styles.exhibitionsImage1}
+          />
+
+          <EventsSection
+            title="Creative Services"
+            imageSrc1={projectsImage}
+            imageSrc2={projectsImage}
+            links={[
+              { name: 'Our Portfolio', href: '/creative-services#portfolio' },
+              { name: 'Get in Touch', href: '/creative-services#contact' }
+            ]}
+            className={styles.creativeServicesSection}
+            image1ClassName={styles.creativeImage1}
+            image2ClassName={styles.creativeImage2}
+          />
+
+          <EventsSection
+            title="Private Events"
+            imageSrc1={projectsImage}
+            imageSrc2={projectsImage}
+            links={[
+              { name: 'View Details', href: '/private-events#details' },
+              { name: 'Contact Us', href: '/private-events#contact' }
+            ]}
+            className={styles.privateEventsSection}
+            image1ClassName={styles.privateImage1}
+            image2ClassName={styles.privateImage2}
+          />
+
+          <EventsSection
+            title="Audio & Visual"
+            imageSrc1={projectsImage}
+            imageSrc2={projectsImage}
+            links={[
+              { name: 'Explore', href: '/audio-visual#explore' },
+              { name: 'Services', href: '/audio-visual#services' }
+            ]}
+            className={styles.audioVisualSection}
+            image1ClassName={styles.audioVisualImage1}
+            image2ClassName={styles.audioVisualImage2}
+          />
+        </div>
         <HomeCarousel/>
+
       </main>
     </>
   );
--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,7 +2,6 @@ import styles from "./Services.module.css";
 import Head from "next/head";
 import Hero from "@/components/layout/hero/Hero";
 import projectsImage from "@/public/images/projects-image.jpg";
-import EventsSection from "@/app/services/components/events-section/Events-Section";
 
 export default function Services() {
   return (
@@ -15,57 +14,7 @@ export default function Services() {
         titleClassName={styles.servicesTitle}
         paragraphClassName={styles.servicesParagraph}/>
       <main>
-        <div className={styles.eventsContainer}>
-          <EventsSection
-            title="Exhibitions & Trade Shows"
-            imageSrc1={projectsImage}
-            links={[
-              { name: 'View Details', href: '/exhibitions#details' },
-              { name: 'Book Now', href: '/exhibitions#book' }
-            ]}
-            className={styles.exhibitionsSection}
-            image1ClassName={styles.exhibitionsImage1}
-          />
-
-          <EventsSection
-            title="Creative Services"
-            imageSrc1={projectsImage}
-            imageSrc2={projectsImage}
-            links={[
-              { name: 'Our Portfolio', href: '/creative-services#portfolio' },
-              { name: 'Get in Touch', href: '/creative-services#contact' }
-            ]}
-            className={styles.creativeServicesSection}
-            image1ClassName={styles.creativeImage1}
-            image2ClassName={styles.creativeImage2}
-          />
-
-          <EventsSection
-            title="Private Events"
-            imageSrc1={projectsImage}
-            imageSrc2={projectsImage}
-            links={[
-              { name: 'View Details', href: '/private-events#details' },
-              { name: 'Contact Us', href: '/private-events#contact' }
-            ]}
-            className={styles.privateEventsSection}
-            image1ClassName={styles.privateImage1}
-            image2ClassName={styles.privateImage2}
-          />
-
-          <EventsSection
-            title="Audio & Visual"
-            imageSrc1={projectsImage}
-            imageSrc2={projectsImage}
-            links={[
-              { name: 'Explore', href: '/audio-visual#explore' },
-              { name: 'Services', href: '/audio-visual#services' }
-            ]}
-            className={styles.audioVisualSection}
-            image1ClassName={styles.audioVisualImage1}
-            image2ClassName={styles.audioVisualImage2}
-          />
-        </div>
+        Services
       </main>
     </>
   );
