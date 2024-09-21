2c38f4da0111212ffd419ca488fa7b39aa83e6c6
Shay <moeshakasanova@gmail.com>
2024-09-21 16:40:50 +0400

    SEO meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,6 +26,36 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
       <link rel="preload" href="/_next/static/css/app/privacy-policy/Privacy-Policy.module.css" as="style" />
       <link rel="preload" href="/_next/static/css/app/projects/Projects.module.css" as="style" />
       <link rel="preload" href="/_next/static/css/app/services/Services.module.css" as="style" />
+
+      {/* SEO Meta Tags */}
+      <meta name="description" content="Event Management Company In Dubai" />
+      <meta name="keywords" content="events, management, Dubai, Alleyway" />
+      <meta name="author" content="Shay Asanova" />
+
+      {/* Open Graph Tags */}
+      <meta property="og:title" content="Alleyway" />
+      <meta property="og:description" content="Event Management Company In Dubai" />
+      <meta property="og:image" content="URL to your image" />
+      <meta property="og:url" content="https://yourwebsite.com" />
+      <meta property="og:type" content="website" />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Alleyway" />
+      <meta name="twitter:description" content="Event Management Company In Dubai" />
+      <meta name="twitter:image" content="URL to your image" />
+
+      {/* Structured Data */}
+      <script type="application/ld+json">
+      {JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": "Alleyway",
+        "url": "https://yourwebsite.com",
+        "logo": "URL to your logo",
+        //Add image, website url and logo once ready for production
+      })}
+      </script>
     </Head>
     <body className={font.className}>
       <Suspense fallback={<Loader />}>
