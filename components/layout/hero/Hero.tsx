9ed808605b01fba9375da5ba355a48e6e06f4b58
Shay <moeshakasanova@gmail.com>
2024-09-21 15:03:17 +0400

    hero page title motion effect

--- components/layout/hero/Hero.tsx
+++ components/layout/hero/Hero.tsx
@@ -1,15 +1,16 @@
 "use client";
-import styles from './Hero.module.css';
+
 import { motion } from "framer-motion";
 import { StaticImageData } from 'next/image';
 import Logo from '../../logo/Logo';
+import styles from './Hero.module.css';
 
 interface HeroProps {
-  image: string | StaticImageData; 
+  image: string | StaticImageData;
   title: string;
   paragraph: string;
-  titleClassName?: string; 
-  paragraphClassName?: string; 
+  titleClassName?: string;
+  paragraphClassName?: string;
 }
 
 const Hero = ({ image, title, paragraph, titleClassName, paragraphClassName }: HeroProps) => {
@@ -27,8 +28,22 @@ const Hero = ({ image, title, paragraph, titleClassName, paragraphClassName }: H
       <div className={styles.heroContent}>
         <Logo />
         <div className={styles.heroInfo}>
-          <h1 className={`${styles.heroTitle} ${titleClassName || ''}`}>{title}</h1>
-          <p className={`${styles.heroParagraph} ${paragraphClassName || ''}`}>{paragraph}</p>
+          <motion.h1
+            initial={{ x: "100vw", opacity: 0 }} 
+            animate={{ x: 0, opacity: 1 }} 
+            transition={{ type: "tween", duration: 0.7 }}  
+            className={`${styles.heroTitle} ${titleClassName || ''}`}
+          >
+            {title}
+          </motion.h1>
+          <motion.p
+            initial={{ x: "-100vw", opacity: 0 }}  
+            animate={{ x: 0, opacity: 1 }} 
+            transition={{ type: "tween", duration: 0.8, delay: 0.3 }}  
+            className={`${styles.heroParagraph} ${paragraphClassName || ''}`}
+          >
+            {paragraph}
+          </motion.p>
         </div>
       </div>
     </motion.div>
