31351bc7cc979ce4dcbcea48ba9e2f4269857d1c
Shay <moeshakasanova@gmail.com>
2024-09-21 16:37:41 +0400

    preload other css

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,10 @@ const RootLayout: React.FC<RootLayoutProps> = ({ children }) => (
   <html lang="en">
     <Head>
       <link rel="preload" href="/_next/static/css/app/page.css" as="style" />
+      <link rel="preload" href="/_next/static/css/app/contact/Contact.module.css" as="style" />
+      <link rel="preload" href="/_next/static/css/app/privacy-policy/Privacy-Policy.module.css" as="style" />
+      <link rel="preload" href="/_next/static/css/app/projects/Projects.module.css" as="style" />
+      <link rel="preload" href="/_next/static/css/app/services/Services.module.css" as="style" />
     </Head>
     <body className={font.className}>
       <Suspense fallback={<Loader />}>
