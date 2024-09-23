d8bb204b8cbfba8a77205c77c2885fcc707c7c95
Shay <moeshakasanova@gmail.com>
2024-09-23 14:43:39 +0400

    links for services in events sections

--- app/page.tsx
+++ app/page.tsx
@@ -41,8 +41,47 @@ export default function Home() {
               title="Exhibitions & Trade Shows"
               imageSrc1={projectsImage}
               links={[
-                { name: 'View Details', href: '/exhibitions#details' },
-                { name: 'Book Now', href: '/exhibitions#book' }
+                { name: 'Booth Design & Set Up', href: '/services#exhibitions' },
+                { name: 'Logistics Management', href: '/services#exhibitions' },
+                { name: 'On-site Coordination', href: '/services#exhibitions' },
+              ]}
+              className={styles.exhibitionsSection}
+              image1ClassName={styles.exhibitionsImage1}
+            />
+            <DynamicEventsSection
+              title="Private Events"
+              imageSrc1={projectsImage}
+              imageSrc2={projectsImage}
+              links={[
+                { name: 'Anniversary', href: '/services/private-events' },
+                { name: 'Birthday Parties', href: '/services#private-events' },
+                { name: 'Weddings', href: '/services#private-events' },
+                { name: 'Social Events', href: '/services#private-events' },
+              ]}
+              className={styles.exhibitionsSection}
+              image1ClassName={styles.exhibitionsImage1}
+            />
+            <DynamicEventsSection
+              title="Creative Services"
+              imageSrc1={projectsImage}
+              imageSrc2={projectsImage}
+              links={[
+                { name: 'Event Concept & Design', href: '/services#creative-services' },
+                { name: 'Thematic Decor', href: '/services#creative-services' },
+                { name: 'Entertainment Planning', href: '/services#creative-services' }
+              ]}
+              className={styles.exhibitionsSection}
+              image1ClassName={styles.exhibitionsImage1}
+            />
+            <DynamicEventsSection
+              title="Audio and Visual Services"
+              imageSrc1={projectsImage}
+              imageSrc2={projectsImage}
+              links={[
+                { name: 'Sound Systems', href: '/services#audio-visual' },
+                { name: 'Lighting Design', href: '/services#audio-visual' },
+                { name: 'LED Screens', href: '/services#audio-visual' },
+                { name: 'Technical Support', href: '/services#audio-visual' },
               ]}
               className={styles.exhibitionsSection}
               image1ClassName={styles.exhibitionsImage1}
