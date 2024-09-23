6e7d4897e970a6bd9f1bdb7928d22f449f6c8d38
Shay <moeshakasanova@gmail.com>
2024-09-23 16:50:22 +0400

    refactored events component, for custom styling of events sections

--- app/components/events-section/Events-Section.tsx
+++ app/components/events-section/Events-Section.tsx
@@ -1,43 +1,79 @@
-// import styles from "./Events-Section.module.css";
+import styles from "./Events-Section.module.css";
 import ImageBox from '@/components/common/image-box/Image-Box';
 import { StaticImageData } from 'next/image';
+import projectImage1 from '@/public/images/projects-image.jpg';
+import projectImage2 from '@/public/images/projects-image.jpg';
 
 interface EventSectionsProps {
   title: string;
-  imageSrc1: StaticImageData;
-  imageSrc2?: StaticImageData; 
-  links: { name: string; href: string }[];
-  className?: string;
-  image1ClassName?: string;
-  image2ClassName?: string;
-  linksClassName?: string;  
+  eventType: 'exhibitions' | 'private-events' | 'creative-services' | 'audio-visual';
 }
 
-const EventsSection: React.FC<EventSectionsProps> = ({
-  title,
-  imageSrc1,
-  imageSrc2,
-  links,
-  className,
-  image1ClassName,
-  image2ClassName,
-  linksClassName
-}) => {
+const EventsSection: React.FC<EventSectionsProps> = ({ title, eventType }) => {
+  const getSectionContent = () => {
+    switch (eventType) {
+      case 'exhibitions':
+        return {
+          images: [projectImage1],
+          links: [
+            { name: 'Booth Design & Set Up', href: '/services#exhibitions' },
+            { name: 'Logistics Management', href: '/services#exhibitions' },
+            { name: 'On-site Coordination', href: '/services#exhibitions' },
+          ],
+        };
+      case 'private-events':
+        return {
+          images: [projectImage1,projectImage2],
+          links: [
+            { name: 'Anniversary', href: '/services/private-events' },
+            { name: 'Birthday Parties', href: '/services#private-events' },
+            { name: 'Weddings', href: '/services#private-events' },
+            { name: 'Social Events', href: '/services#private-events' },
+          ],
+        };
+      case 'creative-services':
+        return {
+          images: [projectImage1, projectImage2],
+          links: [
+            { name: 'Event Concept & Design', href: '/services#creative-services' },
+            { name: 'Thematic Decor', href: '/services#creative-services' },
+            { name: 'Entertainment Planning', href: '/services#creative-services' },
+          ],
+        };
+      case 'audio-visual':
+        return {
+          images: [projectImage1, projectImage2],
+          links: [
+            { name: 'Sound Systems', href: '/services#audio-visual' },
+            { name: 'Lighting Design', href: '/services#audio-visual' },
+            { name: 'LED Screens', href: '/services#audio-visual' },
+            { name: 'Technical Support', href: '/services#audio-visual' },
+          ],
+        };
+      default:
+        return { images: [], links: [] };
+    }
+  };
+
+  const sectionContent = getSectionContent();
+
   return (
-    <section className={`${className}`}>
+    <section className={styles[`${eventType}Container`]}>
       <h2>{title}</h2>
-      <div>
-        <ImageBox imageSrc={imageSrc1} className={image1ClassName} />
-        {imageSrc2 && <ImageBox imageSrc={imageSrc2} className={image2ClassName} />}
+      <div className={styles.imageAndLinksContainer}>
+        <div className={styles.imagesContainer}>
+          {sectionContent.images.map((src, index) => (
+            <ImageBox key={index} imageSrc={src as StaticImageData} className={styles[`${eventType}Image${index + 1}`]} />
+          ))}
+        </div>
         <ul>
-          {links.map((link, index) => (
-            <li key={index} className={linksClassName}>
+          {sectionContent.links.map((link, index) => (
+            <li key={index}>
               <a href={link.href}>{link.name}</a>
             </li>
           ))}
         </ul>
       </div>
-     
     </section>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import React, { Suspense } from 'react';
 import Head from 'next/head';
 import Loader from '@/components/common/loader/Loader';
 import homeImage from "@/public/images/header-home.jpg";
-import projectsImage from "@/public/images/projects-image.jpg";
 import dynamic from 'next/dynamic';
 
 const DynamicHero = dynamic(() => import('@/components/layout/hero/Hero'), {
@@ -37,62 +36,10 @@ export default function Home() {
       <main>
         <div className={styles.eventsContainer}>
           <Suspense fallback={<Loader  />}>
-            <DynamicEventsSection
-              title="Exhibitions & Trade Shows"
-              imageSrc1={projectsImage}
-              links={[
-                { name: 'Booth Design & Set Up', href: '/services#exhibitions' },
-                { name: 'Logistics Management', href: '/services#exhibitions' },
-                { name: 'On-site Coordination', href: '/services#exhibitions' },
-              ]}
-              className={styles.exhibitionsSection}
-              image1ClassName={styles.exhibitionsImage1}
-              linksClassName={styles.sectionLinks}
-            />
-            <DynamicEventsSection
-              title="Private Events"
-              imageSrc1={projectsImage}
-              imageSrc2={projectsImage}
-              links={[
-                { name: 'Anniversary', href: '/services/private-events' },
-                { name: 'Birthday Parties', href: '/services#private-events' },
-                { name: 'Weddings', href: '/services#private-events' },
-                { name: 'Social Events', href: '/services#private-events' },
-              ]}
-              className={styles.privateEventsSection}
-              image1ClassName={styles.privateEventsImage1}
-              image2ClassName={styles.privateEventsImage2}
-              linksClassName={styles.sectionLinks}
-            />
-            <DynamicEventsSection
-              title="Creative Services"
-              imageSrc1={projectsImage}
-              imageSrc2={projectsImage}
-              links={[
-                { name: 'Event Concept & Design', href: '/services#creative-services' },
-                { name: 'Thematic Decor', href: '/services#creative-services' },
-                { name: 'Entertainment Planning', href: '/services#creative-services' }
-              ]}
-              className={styles.creativeServicesSection}
-              image1ClassName={styles.creativeServicesImage1}
-              image2ClassName={styles.creativeServicesImage2}
-              linksClassName={styles.sectionLinks}
-            />
-            <DynamicEventsSection
-              title="Audio and Visual Services"
-              imageSrc1={projectsImage}
-              imageSrc2={projectsImage}
-              links={[
-                { name: 'Sound Systems', href: '/services#audio-visual' },
-                { name: 'Lighting Design', href: '/services#audio-visual' },
-                { name: 'LED Screens', href: '/services#audio-visual' },
-                { name: 'Technical Support', href: '/services#audio-visual' },
-              ]}
-              className={styles.audioVisualSection}
-              image1ClassName={styles.audioVisualImage1}
-              image2ClassName={styles.audioVisualImage2}
-              linksClassName={styles.sectionLinks}
-            />
+            <DynamicEventsSection title="Exhibitions & Trade Shows" eventType="exhibitions" />
+            <DynamicEventsSection title="Private Events" eventType="private-events" />
+            <DynamicEventsSection title="Creative Services" eventType="creative-services" />
+            <DynamicEventsSection title="Audio and Visual Services" eventType="audio-visual" />
           </Suspense>
         </div>
         <Suspense fallback={<Loader />}>
