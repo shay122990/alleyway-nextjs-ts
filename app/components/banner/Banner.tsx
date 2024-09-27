ab63095977280111ed2fc5a4be7ff4980b3a82c1
Shay <moeshakasanova@gmail.com>
2024-09-27 19:20:57 +0400

    animated side arrow in banner

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -1,4 +1,6 @@
+"use client"
 import Link from 'next/link';
+import { motion } from 'framer-motion';
 import styles from "./Banner.module.css";
 import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md';
 
@@ -6,8 +8,18 @@ const Banner = () => {
   return (
     <div className={styles.bannerContainer}>
       <Link href="/services#other-services" scroll={false} className={styles.bannerContent}>
-          <p>Our Services Are Limitless</p>
-          <MdOutlineKeyboardDoubleArrowRight className={styles.bannerSideArrow} />
+        <p>Our Services Are Limitless</p>
+        <motion.div
+          className={styles.bannerSideArrow}
+          animate={{ scale: [1, 1.5, 1] }}
+          transition={{ 
+            duration: 1.5, 
+            repeat: Infinity, 
+            ease: "easeInOut" 
+          }}
+        >
+          <MdOutlineKeyboardDoubleArrowRight />
+        </motion.div>
       </Link>
     </div>
   );
