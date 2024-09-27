b615dcbe26549cc4292e19b6f080713fdf5522d1
Shay <moeshakasanova@gmail.com>
2024-09-27 19:15:47 +0400

    linking banner to services page

--- app/components/banner/Banner.tsx
+++ app/components/banner/Banner.tsx
@@ -1,16 +1,16 @@
-import styles from "./Banner.module.css"
+import Link from 'next/link';
+import styles from "./Banner.module.css";
 import { MdOutlineKeyboardDoubleArrowRight } from 'react-icons/md';
-
 
 const Banner = () => {
   return (
     <div className={styles.bannerContainer}>
-        <div className={styles.bannerContent}>
-          <p>Our Services Are limitless</p>
-          <MdOutlineKeyboardDoubleArrowRight  className={styles.bannerSideArrow} />
-        </div>
+      <Link href="/services#other-services" scroll={false} className={styles.bannerContent}>
+          <p>Our Services Are Limitless</p>
+          <MdOutlineKeyboardDoubleArrowRight className={styles.bannerSideArrow} />
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
