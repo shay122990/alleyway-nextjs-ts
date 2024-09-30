54d1c323763634b1524850f0c05a2d541f070c7b
Shay <moeshakasanova@gmail.com>
2024-10-01 01:59:22 +0400

    hero styling

--- components/layout/hero/Hero.tsx
+++ components/layout/hero/Hero.tsx
@@ -1,59 +1,3 @@
-// "use client";
-// import styles from './Hero.module.css';
-// import { motion } from "framer-motion";
-// import { StaticImageData } from 'next/image';
-// import Logo from "@/components/logo/Logo"
-
-
-// interface HeroProps {
-//   image: string | StaticImageData;
-//   title: string;
-//   paragraph: string;
-//   titleClassName?: string;
-//   paragraphClassName?: string;
-// }
-
-// const Hero = ({ image, title, paragraph, titleClassName, paragraphClassName }: HeroProps) => {
-//   const backgroundImage = typeof image === 'string' ? image : image.src;
-
-//   return (
-//     <motion.div 
-//       initial={{ opacity: 0 }}
-//       animate={{ opacity: 1 }}
-//       exit={{ opacity: 0 }}
-//       transition={{ duration: 0.5 }}
-//       className={styles.hero}
-//       style={{ backgroundImage: `url(${backgroundImage})` }}
-//     > 
-//       <div className={styles.heroContent}>
-//           <Logo />
-//         <div className={styles.heroInfo}>
-//           <motion.h1
-//             initial={{ x: "100vw", opacity: 0 }} 
-//             animate={{ x: 0, opacity: 1 }} 
-//             transition={{ type: "tween", duration: 0.7 }}  
-//             className={`${styles.heroTitle} ${titleClassName}`}
-//           >
-//             {title}
-//           </motion.h1>
-//           <motion.p
-//             initial={{ x: "-100vw", opacity: 0 }}  
-//             animate={{ x: 0, opacity: 1 }} 
-//             transition={{ type: "tween", duration: 0.8, delay: 0.3 }}  
-//             className={`${styles.heroParagraph} ${paragraphClassName}`}
-//           >
-//             {paragraph}
-//           </motion.p>
-//         </div>
-//       </div>
-//     </motion.div>
-//   );
-// };
-
-// export default Hero;
-
-// components/layout/hero/Hero.tsx
-
 "use client";
 import styles from './Hero.module.css';
 import { motion } from "framer-motion";
@@ -74,10 +18,6 @@ const Hero = ({ video, image, title, paragraph, titleClassName, paragraphClassNa
 
   return (
     <motion.div 
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0 }}
-      transition={{ duration: 0.5 }}
       className={styles.hero}
     > 
       {video && (
