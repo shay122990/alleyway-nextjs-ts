06e4bb6b79bc134e01cee6fddfd0b72889622308
Shay <moeshakasanova@gmail.com>
2024-10-09 19:07:32 +0400

    moved projects page data

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -3,11 +3,7 @@ import Head from 'next/head';
 import aboutImage from "@/public/images/about-image.jpg";
 import Hero from "@/components/layout/hero/Hero";
 import ImageBox from "@/components/common/image-box/Image-Box";
-import projectImage1 from '@/public/images/projects-image1.jpg';
-import projectImage2 from '@/public/images/projects-image2.jpg';
-import projectImage3 from '@/public/images/projects-image3.jpg';
-import projectImage4 from '@/public/images/projects-image4.jpg';
-
+import { projectsPageImages } from '@/data/ProjectsPageData'; 
 
 export default function Project() {
   return (
@@ -16,58 +12,58 @@ export default function Project() {
         <title>Projects - Alleyway</title>
         <meta name="description" content="The projects we've successfully completed" />
       </Head>
-        <Hero 
-          image={aboutImage} 
-          title="Always up to the challenge!" 
-          paragraph="The Projects we've successfully completed"
-          titleClassName={styles.projectsTitle}
-          paragraphClassName={styles.projectsParagraph}
-        />
+      <Hero 
+        image={aboutImage} 
+        title="Always up to the challenge!" 
+        paragraph="The Projects we've successfully completed"
+        titleClassName={styles.projectsTitle}
+        paragraphClassName={styles.projectsParagraph}
+      />
       <main className={styles.main}>
-          <div className={styles.projectSection} id="exhibitions&tradeshows">
-            <div className={styles.section1ProjectsImages}>
-              <ImageBox imageSrc={projectImage1}/>
-              <ImageBox imageSrc={projectImage2}/>
-              <ImageBox imageSrc={projectImage3}/>
-              <h2>The flawless design with effortless solutions</h2>
-            </div>
+        <div className={styles.projectSection} id="exhibitions&tradeshows">
+          <div className={styles.section1ProjectsImages}>
+            <ImageBox imageSrc={projectsPageImages.image1} />
+            <ImageBox imageSrc={projectsPageImages.image2} />
+            <ImageBox imageSrc={projectsPageImages.image3} />
+            <h2>The flawless design with effortless solutions</h2>
           </div>
-          <div className={styles.projectSection}>
-            <div className={styles.section2ProjectsImages}>
-              <ImageBox imageSrc={projectImage1}/>
-              <div className={styles.section2Info}>
+        </div>
+        <div className={styles.projectSection}>
+          <div className={styles.section2ProjectsImages}>
+            <ImageBox imageSrc={projectsPageImages.image1} />
+            <div className={styles.section2Info}>
               <h2>The experience</h2>
               <p>Praesent efficitur sodales elit, sed porta nisl dignissim eget. Fusce vitae pretium ante.</p> 
-              </div>
             </div>
           </div>
-          <hr className={styles.section2Hr}/>
-          <div className={styles.projectSection} id="private-events">
-            <div className={styles.section3ProjectsImages}>
-              <ImageBox imageSrc={projectImage1}/>
-              <ImageBox imageSrc={projectImage2}/>
-              <ImageBox imageSrc={projectImage3}/>
-            </div>
+        </div>
+        <hr className={styles.section2Hr} />
+        <div className={styles.projectSection} id="private-events">
+          <div className={styles.section3ProjectsImages}>
+            <ImageBox imageSrc={projectsPageImages.image1} />
+            <ImageBox imageSrc={projectsPageImages.image2} />
+            <ImageBox imageSrc={projectsPageImages.image3} />
           </div>
-          <div className={styles.projectSection} id="creative-services">
-            <div className={styles.section4ProjectsImages}>
-              <div className={styles.section4Info}>
-                <h2>Stage Up</h2>
-                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
-              </div>
-              <ImageBox imageSrc={projectImage1}/>
+        </div>
+        <div className={styles.projectSection} id="creative-services">
+          <div className={styles.section4ProjectsImages}>
+            <div className={styles.section4Info}>
+              <h2>Stage Up</h2>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
             </div>
+            <ImageBox imageSrc={projectsPageImages.image1} />
           </div>
-          <div className={styles.projectSection} id="audio&visual">
-            <hr className={styles.section5Hr}/>
-            <div className={styles.section5ProjectsImages}>
-              <ImageBox imageSrc={projectImage1}/>
-              <ImageBox imageSrc={projectImage2}/>
-              <ImageBox imageSrc={projectImage3}/>
-              <ImageBox imageSrc={projectImage4}/>
-            </div>
+        </div>
+        <div className={styles.projectSection} id="audio&visual">
+          <hr className={styles.section5Hr} />
+          <div className={styles.section5ProjectsImages}>
+            <ImageBox imageSrc={projectsPageImages.image1} />
+            <ImageBox imageSrc={projectsPageImages.image2} />
+            <ImageBox imageSrc={projectsPageImages.image3} />
+            <ImageBox imageSrc={projectsPageImages.image4} />
           </div>
+        </div>
       </main>
     </>
   );
---
+++ data/ProjectsPageData.js
@@ -0,0 +1,11 @@
+import projectImage1 from "@/public/images/projects-image1.jpg";
+import projectImage2 from "@/public/images/projects-image2.jpg";
+import projectImage3 from "@/public/images/projects-image3.jpg";
+import projectImage4 from "@/public/images/projects-image4.jpg";
+
+export const projectsPageImages = {
+  image1: projectImage1,
+  image2: projectImage2,
+  image3: projectImage3,
+  image4: projectImage4,
+};
