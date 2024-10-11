530b8960f206b0d9fdd6ca0ff7a2254d0350a485
Shay <moeshakasanova@gmail.com>
2024-10-11 17:38:44 +0400

    disabled ssr for no essential components

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,71 @@
+// import styles from "./page.module.css";
+// import Head from 'next/head';
+// import Hero from "@/components/layout/hero/Hero";
+// import EventsSection from "./components/events-section/Events-Section";
+// import Banner from "./components/banner/Banner";
+// import MasonryGrid from "./components/masonry-grid/MasonryGrid";
+// import homeHero from "@/public/images/homeHero.webp"
+
+// export default function Home() {
+
+//   return (
+//     <>
+//       <Head>
+//         <title>Alleyway - Plan Any Event</title>
+//         <meta name="description" content="Alleyway - Plan Any Event With Us " />
+//         <link rel="preload" href={homeHero.src} as="image" />
+//       </Head>
+//         <Hero 
+//           width={1920}
+//           height={1440}
+//           image={homeHero}
+//           title="The Events Management Company" 
+//           paragraph="We provide top-notch services to meet your needs." 
+//           titleClassName={styles.homeTitle}
+//           paragraphClassName={styles.homeParagraph} 
+//         />
+//         <main className={styles.main}>
+//           <EventsSection />
+//           <Banner />
+//           <MasonryGrid />
+//         </main>
+//     </>
+//   );
+// }
+
+
 import styles from "./page.module.css";
 import Head from 'next/head';
 import Hero from "@/components/layout/hero/Hero";
-import EventsSection from "./components/events-section/Events-Section";
-import Banner from "./components/banner/Banner";
-import MasonryGrid from "./components/masonry-grid/MasonryGrid";
-import homeHero from "@/public/images/homeHero.webp"
+import dynamic from 'next/dynamic';
+import homeHero from "@/public/images/homeHero.webp";
+
+const EventsSection = dynamic(() => import("./components/events-section/Events-Section"), { ssr: false });
+const Banner = dynamic(() => import("./components/banner/Banner"), { ssr: false });
+const MasonryGrid = dynamic(() => import("./components/masonry-grid/MasonryGrid"), { ssr: false });
 
 export default function Home() {
-
   return (
     <>
       <Head>
         <title>Alleyway - Plan Any Event</title>
-        <meta name="description" content="Alleyway - Plan Any Event With Us " />
+        <meta name="description" content="Alleyway - Plan Any Event With Us" />
         <link rel="preload" href={homeHero.src} as="image" />
       </Head>
-        <Hero 
-          width={1920}
-          height={1440}
-          image={homeHero}
-          title="The Events Management Company" 
-          paragraph="We provide top-notch services to meet your needs." 
-          titleClassName={styles.homeTitle}
-          paragraphClassName={styles.homeParagraph} 
-        />
-        <main className={styles.main}>
-          <EventsSection />
-          <Banner />
-          <MasonryGrid />
-        </main>
+      <Hero 
+        width={1920}
+        height={1440}
+        image={homeHero}
+        title="The Events Management Company" 
+        paragraph="We provide top-notch services to meet your needs." 
+        titleClassName={styles.homeTitle}
+        paragraphClassName={styles.homeParagraph} 
+      />
+      <main className={styles.main}>
+        <EventsSection />
+        <Banner />
+        <MasonryGrid />
+      </main>
     </>
   );
 }
