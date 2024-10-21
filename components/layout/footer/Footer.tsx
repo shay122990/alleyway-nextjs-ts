37e10a2dcf682cec9502161f8e2760afa0f4767c
Shay <moeshakasanova@gmail.com>
2024-10-21 21:35:37 +0400

    separated links from footer

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -1,28 +1,11 @@
-'use client';
-
-import { usePathname } from 'next/navigation'; 
-import Icons from "@/components/common/icons/Icons";
 import styles from "./Footer.module.css";
+import Icons from "@/components/common/icons/Icons";
 import Logo from "@/components/logo/Logo";
 import Button from "@/components/common/buttons/Button";
-import Links from "@/components/common/links/Links"
+import Links from "@/components/common/links/Links";
+import FooterNavLinks from '../../navigation/footer-nav-items/Footer-Nav-Links';  
 
 export default function Footer() {
-  const pathname = usePathname(); 
-
-  const footerNavItems = [
-    { href: '/services/#about-us', label: 'About Us' },
-    { href: '/contact', label: 'Contact Us' },
-    { href: '/privacy-policy', label: 'Privacy Policy' },
-    { href: '/services/#exhibitions&tradeshows', label: 'Exhibitions & Trade Shows' },
-    { href: '/services/#private-events', label: 'Private Events' },
-    { href: '/services/#creative-services', label: 'Creative Services' },
-    { href: '/services/#audio-visual', label: 'Audio & Visual' }
-  ];
-
-  const companyLinks = footerNavItems.slice(0, 3);
-  const serviceLinks = footerNavItems.slice(3);
-
   return (
     <footer className={styles.footerContainer}>
       <div className={styles.logoIconContainer}>
@@ -44,28 +27,7 @@ export default function Footer() {
             <Button buttonType="primary">Book Us</Button>
           </Links>
         </div>
-        <div className={styles.linksContainer}>
-          <div className={styles.company}>
-            <h4>Company</h4>
-            <ul>
-              {companyLinks.map(({ href, label }, index) => (
-                <li key={index} className={pathname === href ? styles.active : ''}>
-                  <Links href={href}>{label}</Links> 
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.services}>
-            <h4>Services</h4>
-            <ul>
-              {serviceLinks.map(({ href, label }, index) => (
-                <li key={index} className={pathname === href ? styles.active : ''}>
-                  <Links href={href}>{label}</Links> 
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
+        <FooterNavLinks />  
       </div>
     </footer>
   );
---
+++ components/navigation/footer-nav-items/Footer-Nav-Links.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import Links from "@/components/common/links/Links";
+import styles from "../../layout/footer/Footer.module.css";
+
+const footerNavItems = [
+  { href: '/services/#about-us', label: 'About Us' },
+  { href: '/contact', label: 'Contact Us' },
+  { href: '/privacy-policy', label: 'Privacy Policy' },
+  { href: '/services/#exhibitions&tradeshows', label: 'Exhibitions & Trade Shows' },
+  { href: '/services/#private-events', label: 'Private Events' },
+  { href: '/services/#creative-services', label: 'Creative Services' },
+  { href: '/services/#audio-visual', label: 'Audio & Visual' }
+];
+
+const companyLinks = footerNavItems.slice(0, 3);
+const serviceLinks = footerNavItems.slice(3);
+
+export default function FooterNavLinks() {
+  const pathname = usePathname();
+
+  return (
+    <div className={styles.linksContainer}>
+      <div className={styles.company}>
+        <h4>Company</h4>
+        <ul>
+          {companyLinks.map(({ href, label }, index) => (
+            <li key={index} className={pathname === href ? styles.active : ''}>
+              <Links href={href}>{label}</Links>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className={styles.services}>
+        <h4>Services</h4>
+        <ul>
+          {serviceLinks.map(({ href, label }, index) => (
+            <li key={index} className={pathname === href ? styles.active : ''}>
+              <Links href={href}>{label}</Links>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
