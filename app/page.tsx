1f07de8d3180b0c449c60cbc418c1ac7c64bc541
Shay <moeshakasanova@gmail.com>
2024-10-22 15:11:02 +0400

    removed dynamic import that fixed layout shift of footer on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import styles from "./page.module.css";
 import Head from 'next/head';
 import Hero from "@/components/layout/hero/Hero";
-import dynamic from 'next/dynamic';
 import homeHero from "@/public/images/homeHero.webp";
+import EventsSection from "./components/events-section/Events-Section";
+import Banner from "./components/banner/Banner";
+import MasonryGrid from "./components/masonry-grid/MasonryGrid";
 
-const EventsSection = dynamic(() => import("./components/events-section/Events-Section"), { ssr: false });
-const Banner = dynamic(() => import("./components/banner/Banner"), { ssr: false });
-const MasonryGrid = dynamic(() => import("./components/masonry-grid/MasonryGrid"), { ssr: false });
 
 export default function Home() {
   return (
@@ -14,12 +13,6 @@ export default function Home() {
       <Head>
         <title>Alleyway - Event & Project Management Company in Dubai</title>
         <meta name="description" content="Alleyway - Plan Any Event & Project With Us" />
-        {/* <link rel="preload" href={homeHero.src} as="image" />
-        <meta property="og:title" content="Alleyway - Event & Project Management Company in Dubai" />
-        <meta property="og:description" content="Alleyway - Plan Any Event & Project With Us" />
-        <meta property="og:image" content={homeHero.src} />
-        <meta property="og:url" content="https://alleywaydxb.com" />
-        <meta property="og:type" content="website" /> */}
       </Head>
       <Hero 
         width={1920}
