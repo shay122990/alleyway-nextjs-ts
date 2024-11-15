b9208d957d9a820e40e3671a7cc3988a5bc60131
Shay <moeshakasanova@gmail.com>
2024-11-15 06:19:05 +0400

    replaced icons in carousel component

--- app/services/components/carousel/Carousel.tsx
+++ app/services/components/carousel/Carousel.tsx
@@ -1,9 +1,9 @@
-// Carousel.tsx
 "use client";
 import React, { useState } from "react";
 import styles from "./Carousel.module.css";
 import { StaticImageData } from "next/image"; 
 import ImageBox from "@/components/common/image-box/Image-Box";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 
 interface CarouselProps {
   images: { src: StaticImageData }[];
@@ -23,7 +23,7 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
   return (
     <div className={styles.carouselContainer}>
       <button className={styles.prevButton} onClick={prevImage}>
-        &#10094;
+        <BsChevronCompactLeft />
       </button>
       <ImageBox
         className={styles.carouselImage}
@@ -31,11 +31,10 @@ const Carousel: React.FC<CarouselProps> = ({ images }) => {
         altText={`Image ${currentIndex + 1}`}
       />
       <button className={styles.nextButton} onClick={nextImage}>
-        &#10095;
+        <BsChevronCompactRight />
       </button>
     </div>
   );
 };
-
 
 export default Carousel;
