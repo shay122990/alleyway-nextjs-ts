90e998c97b4c93e6f0d1cf4459e9b8bd9de5d1e5
shay asanova <moeshakasanova@gmail.com>
2025-09-18 14:29:26 +0400

    update on send-email

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,27 +1,29 @@
-import emailjs from 'emailjs-com';
+import emailjs from "emailjs-com";
 
-export const sendEmail = async (data: { name: string; email: string; message: string }) => {
-  const { name, email, message } = data;
-
+export const sendEmail = async (data: {
+  name: string;
+  email: string;
+  message: string;
+}) => {
   try {
     const response = await emailjs.send(
-      'service_1y8x8jd', 
-      'template_pz2pz5f', 
+      "service_1y8x8jd",
+      "template_pz2pz5f",
       {
-        name,
-        email,
-        message,
+        from_name: data.name,
+        reply_to: data.email,
+        message: data.message,
       },
-      '3VxZ2XS6rmTPLzTwV'
+      "3VxZ2XS6rmTPLzTwV"
     );
 
-    if (response.status !== 200) { 
-      throw new Error('Failed to send email');
+    if (response.status !== 200) {
+      throw new Error("Failed to send email");
     }
 
-    console.log('SUCCESS!', response.status, response.text);
+    console.log("SUCCESS!", response.status, response.text);
   } catch (error) {
-    console.error('FAILED...', error);
+    console.error("FAILED...", error);
     throw error;
   }
 };
