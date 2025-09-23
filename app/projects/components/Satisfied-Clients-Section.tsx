d22fdfc753aff14b8cf5979bf5de344db0851080
shay asanova <moeshakasanova@gmail.com>
2025-09-23 14:38:51 +0400

    exported clientlogo data to projectsdata page

--- app/projects/components/Satisfied-Clients-Section.tsx
+++ app/projects/components/Satisfied-Clients-Section.tsx
@@ -3,74 +3,7 @@ import { useRef } from "react";
 import Image from "next/image";
 import { FaChevronDown, FaArrowRight } from "react-icons/fa";
 import Link from "next/link";
-
-const clientLogos = [
-  "/images/client-logos/img1.png",
-  "/images/client-logos/img2.jpg",
-  "/images/client-logos/img3.jpg",
-  "/images/client-logos/img4.jpg",
-  "/images/client-logos/img5.jpg",
-  "/images/client-logos/img6.webp",
-  "/images/client-logos/img7.png",
-  "/images/client-logos/img8.svg",
-  "/images/client-logos/img9.png",
-  "/images/client-logos/img10.png",
-  "/images/client-logos/img11.jpg",
-  "/images/client-logos/img12.jpg",
-  "/images/client-logos/img13.png",
-  "/images/client-logos/img14.png",
-  "/images/client-logos/img15.svg",
-  "/images/client-logos/img16.png",
-  "/images/client-logos/img17.png",
-  "/images/client-logos/img18.png",
-  "/images/client-logos/img19.png",
-  "/images/client-logos/img20.webp",
-  "/images/client-logos/img21.png",
-  "/images/client-logos/img22.png",
-  "/images/client-logos/img23.svg",
-  "/images/client-logos/img24.png",
-  "/images/client-logos/img25.png",
-  "/images/client-logos/img26.png",
-  "/images/client-logos/img27.png",
-  "/images/client-logos/img28.png",
-  "/images/client-logos/img29.png",
-  "/images/client-logos/img30.png",
-  "/images/client-logos/img31.png",
-  "/images/client-logos/img32.png",
-  "/images/client-logos/img33.png",
-  "/images/client-logos/img34.avif",
-  "/images/client-logos/img35.png",
-  "/images/client-logos/img36.png",
-  "/images/client-logos/img37.png",
-  "/images/client-logos/img38.png",
-  "/images/client-logos/img39.png",
-  "/images/client-logos/img40.png",
-  "/images/client-logos/img41.png",
-  "/images/client-logos/img42.png",
-  "/images/client-logos/img43.png",
-  "/images/client-logos/img44.png",
-  "/images/client-logos/img45.png",
-  "/images/client-logos/img46.png",
-  "/images/client-logos/img47.png",
-  "/images/client-logos/img48.png",
-  "/images/client-logos/img49.png",
-  "/images/client-logos/img50.png",
-  "/images/client-logos/img51.webp",
-  "/images/client-logos/img52.jpg",
-  "/images/client-logos/img53.png",
-  "/images/client-logos/img54.png",
-  "/images/client-logos/img55.png",
-  "/images/client-logos/img56.png",
-  "/images/client-logos/img57.jpg",
-  "/images/client-logos/img58.webp",
-  "/images/client-logos/img59.png",
-  "/images/client-logos/img60.png",
-  "/images/client-logos/img61.png",
-  "/images/client-logos/img62.png",
-  "/images/client-logos/img63.png",
-  "/images/client-logos/img64.png",
-  "/images/client-logos/img65.png",
-];
+import { clientLogos } from "@/data/ProjectsPageData";
 
 export default function SatisfiedClientsSection() {
   const scrollRef = useRef<HTMLDivElement>(null);
@@ -113,7 +46,7 @@ export default function SatisfiedClientsSection() {
             ))}
           </div>
 
-          {/* DESKTOP/TABLET: masonry  */}
+          {/* DESKTOP/TABLET: masonry */}
           <div className="hidden md:block">
             <div className="columns-4 lg:columns-5 space-y-2">
               {clientLogos.map((logo, i) => (
