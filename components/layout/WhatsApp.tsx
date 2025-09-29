304a784ffc6e09317a28f2635da0feafb32ef2c8
shay asanova <moeshakasanova@gmail.com>
2025-09-29 08:00:03 +0400

    anchor tag instead of button for accessibility on whatapp floating icon

--- components/layout/WhatsApp.tsx
+++ components/layout/WhatsApp.tsx
@@ -6,25 +6,31 @@ import { FaWhatsapp } from "react-icons/fa";
 interface WhatsAppProps {
   className?: string;
   isFloating?: boolean;
-  nonce?: string;
 }
 
-const WhatsApp: React.FC<WhatsAppProps> = ({ className = '', isFloating = false, nonce }) => {
+const WhatsApp: React.FC<WhatsAppProps> = ({
+  className = "",
+  isFloating = false,
+}) => {
   const whatsappURL = "https://wa.me/+971528182368";
 
-  const handleClick = () => {
-    window.open(whatsappURL, "_blank", "noopener,noreferrer");
-  };
-
   return (
-    <button
-      onClick={handleClick}
+    <a
+      href={whatsappURL}
+      target="_blank"
+      rel="noopener noreferrer"
       aria-label="Open WhatsApp"
       className={`
         ${className}
-        ${isFloating ? `hidden md:inline fixed left-5 top-[80%] -translate-y-1/2 z-[1000] bg-transparent text-tealCustom text-[43px] cursor-pointer border-none transition-transform duration-300 hover:scale-125` : ""}`}>
+        ${
+          isFloating
+            ? `hidden md:inline fixed left-5 top-[80%] -translate-y-1/2 z-[1000] bg-transparent text-tealCustom text-[43px] cursor-pointer transition-transform duration-300 hover:scale-125`
+            : ""
+        }
+      `}
+    >
       <FaWhatsapp />
-    </button>
+    </a>
   );
 };
 
